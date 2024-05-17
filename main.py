9e135c4d95f0b7ffdf53da96dc2cc1a726409314
Hillary Y <107068896+hillaryyang@users.noreply.github.com>
2024-05-17 18:20:38 -0400

    adding new cube

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import copy
 import numpy as np
 from itertools import product
-import time
+import random
 import turns
 from turns import Cube
 
-start = time.time()
-
+# applies the sequence "moves" onto the cube "cb"
 def seq(cb, moves):
     for mv in moves:
+        #if the move contains M, then deal with it separately
         if "M" in mv:
             if mv == "M\'":
                 cb.move("R", 0, -1)
@@ -24,48 +25,51 @@ def seq(cb, moves):
         else:
             dir = 1
             if len(mv) != 1:
-                dir = 2 if mv[1] == "2" else 1 if "'" not in mv[0] else -1
+                dir = 2 if mv[1] == "2" else 1 if "\'" not in mv[1] else -1
             cb.move(mv[0], 0, dir)
 
-        '''
-        print(ct + 1)
-        ct += 1
-        print(cube.stickers)'''
-    return cb.stickers
+    return cb
 
+# list of moves
 moves = [
+    'M', 'M\'', 'M2',
     'U', 'U\'', 'U2',
     'D', 'D\'', 'D2',
     'L', 'L\'', 'L2',  
     'R', 'R\'', 'R2',
     'F', 'F\'', 'F2',
-    'B', 'B\'', 'B2',
-    'M', 'M\'', 'M2'
+    'B', 'B\'', 'B2'
 ]
 
-#defines cube
+
+# defines cubes
 c = Cube(3)
 temp = Cube(3)
-#stores initial (H perm) and solved state
+
+# stores initial state (H perm) and the solved state
 solved = temp.stickers
-h = ["M2", "U\'", "M2", "U2", "M2", "U\'", "M2"]
-hperm = seq(Cube(3), h)
+
+# defines h perm algorithm
+alg = ["M2", "U\'", "M2", "U2", "M2", "U\'", "M2"]
+
+# applies the h perm to a cube
+hperm = seq(Cube(3), alg)
+
+# initializes array of working sequences
 works = []
 
+# gets all length 7 permutations of moves
 combos = product(moves, repeat=7)
 
-ct = 0
+# iterates over all sequences
 for sequence in combos:
-    newstate = seq(c, sequence)
-    if np.array_equal(newstate, solved):
-        works.append(sequence)
-    
-    #reset c
-    c.stickers = hperm
-    if ct % 100000 == 0:
-        print(ct)
-        print(sequence)
-        print(time.time() - start)
-    ct += 1
+    c = copy.deepcopy(hperm)
 
-print(works)
+    #generates the new state resulting from applying sequence on the cube
+    newstate = seq(c, sequence)
+
+    #if the newstate is the solved state, add it to works
+    if np.array_equal(newstate.stickers, solved):
+        works.append(sequence)
+
+print("Final working solution:", works)
